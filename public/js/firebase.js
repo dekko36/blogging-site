4a7fa88696c74e475bca6ed4bb181a11c097007b
dekko36 <kipkoechderrick36@gmail.com>
2023-11-26 03:56:32 -0800

    actual commit

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+       apiKey: "AIzaSyC6kd7dL3UsJpwzSUcPCkws9EDZouGoV0I",
+        authDomain: "blogging-website-bd407.firebaseapp.com",
+        projectId: "blogging-website-bd407",
+        storageBucket: "blogging-website-bd407.appspot.com",
+        messagingSenderId: "876203546895",
+        appId: "1:876203546895:web:9c75ff3f9f72d84481c67a"
+      };
 };
 
 firebase.initializeApp(firebaseConfig);
